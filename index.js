c4528d2657586db85f223e3f475ad99de50b2374
mahmoud nayel <mahmmoudnayel2004@gmail.com>
2025-10-17 22:24:44 +0300

    vercel25

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const cors = require("cors");
 const dbconnect = require("./config/dbconfig");
 
 dbconnect();
+// ✅ Tell Express to trust Vercel (or any reverse proxy)
+app.set("trust proxy", 1);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
